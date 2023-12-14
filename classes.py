ed0c15128c23956f44549d3eed7c7ca7c6188c5c
Pedro Aparício <pha199@hotmail.com>
2023-12-14 10:15:15 +0000

    Adicionar objeto

--- classes.py
+++ classes.py
@@ -1,5 +1,8 @@
-class Carros
+class Carros:
     def __init__(self, marca, modelo, kms):
         self.marca = marca
         self.modelo = modelo
         self.kms = kms
+
+    def __str__(self):
+        return f"{self.marca} {self.modelo} com {self.kms} kms"
---
+++ okl.py
@@ -0,0 +1,5 @@
+from classes import Carros
+    
+c1 = Carros("Volvo", "XC70", 120000)
+
+print(c1)
